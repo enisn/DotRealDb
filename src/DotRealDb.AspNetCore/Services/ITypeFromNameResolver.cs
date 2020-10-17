73b1c058666fdd9ee31f201b9272ac89a84f1170
enisn <iletisim@enisnecipoglu.com>
2020-10-18 01:46:53 +0300

    🔨 Move GetTypeFromName to interface

--- src/DotRealDb.AspNetCore/Services/ITypeFromNameResolver.cs
+++ src/DotRealDb.AspNetCore/Services/ITypeFromNameResolver.cs
@@ -1,9 +1,13 @@
-﻿namespace DotRealDb.AspNetCore.Services
+﻿using System;
+
+namespace DotRealDb.AspNetCore.Services
 {
     public interface ITypeFromNameResolver
     {
         object Resolve(string name);
 
         T Resolve<T>(string name);
+
+        Type GetTypeFromName(string name);
     }
 }
