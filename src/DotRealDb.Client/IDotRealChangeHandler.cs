47ba8fb2fc745df0a2b343679e09683a9c596ca5
enisn <iletisim@enisnecipoglu.com>
2020-10-18 01:59:53 +0300

    ✨ Implementation of ConnectAndTrack

--- src/DotRealDb.Client/IDotRealChangeHandler.cs
+++ src/DotRealDb.Client/IDotRealChangeHandler.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DotRealDb.Client
 {
     public interface IDotRealChangeHandler
     {
+        Task<ObservableCollection<T>> ConnectAndTrackAsync<T>(string dbContextName, string entityName = null, uint limit = 32);
+
         Task StartTrackingAsync<T>(IList<T> source, string dbContextName, string entityName = default);
     }
 }
