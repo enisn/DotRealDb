0e8ea88453f5e28a6b74dbc271d0f2e4d7d073ac
enisn <iletisim@enisnecipoglu.com>
2020-10-19 21:53:52 +0300

    🐛 Fix triggering database changes

--- src/DotRealDb.AspNetCore/DotRealChangeTracker.cs
+++ src/DotRealDb.AspNetCore/DotRealChangeTracker.cs
@@ -1,11 +1,13 @@
 ﻿using DotRealDb.AspNetCore.Hubs;
 using DotRealDb.AspNetCore.Hubs.Clients;
+using DotRealDb.AspNetCore.Internals;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,7 +26,7 @@ namespace DotRealDb.AspNetCore
 
         public async Task<T> TrackAndPublishAfterAsync<T>(DbContext dbContext, Func<Task<T>> saveChanges)
         {
-            var entries = dbContext.ChangeTracker.Entries().ToArray();
+            var entries = dbContext.ChangeTracker.Entries().Select(EntrySelector).ToArray();
             var result = await saveChanges();
             Publish(entries);
             return result;
@@ -32,23 +34,25 @@ namespace DotRealDb.AspNetCore
 
         public T TrackAndPublishAfter<T>(DbContext dbContext, Func<T> saveChanges)
         {
-            var entries = dbContext.ChangeTracker.Entries().ToArray();
+            var entries = dbContext.ChangeTracker.Entries().Select(EntrySelector).ToArray();
             var result = saveChanges();
             Publish(entries);
             return result;
         }
 
-        private async void Publish(EntityEntry[] entries)
+        private EntityStateSummary EntrySelector(EntityEntry s, int i) => new EntityStateSummary(s.Entity.GetType().Name, s.Context.GetType().Name, s.Entity, s.State);
+
+        private async void Publish(EntityStateSummary[] entries)
         {
             await PublishAsync(entries);
         }
 
-        private async Task PublishAsync(EntityEntry[] entries)
+        private async Task PublishAsync(EntityStateSummary[] entries)
         {
             foreach (var entry in entries)
             {
-                var groupName = entry.Context.GetType().Name + "_" + entry.Entity.GetType().Name;
-                switch (entry.State)
+                var groupName = entry.DbContextName + "_" + entry.EntityName;
+                switch (entry.EntityState)
                 {
                     case EntityState.Deleted:
                         await hubContext.Clients.Group(groupName).Deleted(entry.Entity);
---
+++ src/DotRealDb.AspNetCore/Internals/EntityStateSummary.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotRealDb.AspNetCore.Internals
+{
+    internal class EntityStateSummary
+    {
+        public EntityStateSummary()
+        {
+        }
+
+        public EntityStateSummary(string entityName, string dbContextName, object entity, EntityState entityState)
+        {
+            this.EntityName = entityName;
+            this.DbContextName = dbContextName;
+            this.Entity = entity;
+            this.EntityState = entityState;
+        }
+
+        public string EntityName { get; set; }
+        public string DbContextName { get; set; }
+        public object Entity { get; set; }
+        public EntityState EntityState { get; set; }
+    }
+}
